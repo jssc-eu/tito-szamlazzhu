794d5204aebfbc0f2635ef4d52f70cba177e805d
szabolcsit <szabolcsit@hu.ibm.com>
2021-04-09 09:28:58 +0200

    fix: remove invalid characters from VAT ids

--- src/lib/check-vies.test.ts
+++ src/lib/check-vies.test.ts
@@ -12,4 +12,20 @@ describe('check VIES', () => {
 
     expect(value).toBe(true);
   });
+
+  test('handle VAT ids with whitespaces', async () => {
+    const value = await checkVIES('HU', 'HU 259 663 30');
+
+    expect(value).toBe(true);
+  });
+  test('handle VAT ids with dashes', async () => {
+    const value = await checkVIES('HU', 'HU 259-663-30');
+
+    expect(value).toBe(true);
+  });
+  test('handle VAT ids with messed up formatting', async () => {
+    const value = await checkVIES('HU', 'HU 259-6 63_30');
+
+    expect(value).toBe(true);
+  });
 });
