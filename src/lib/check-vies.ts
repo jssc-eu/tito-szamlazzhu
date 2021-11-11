742645e99143e1215f56046fa7dad2159c4b8b54
szabolcsit <szabolcsit@hu.ibm.com>
2021-11-11 17:39:07 +0100

    chore: replace vies checker

--- src/lib/check-vies.ts
+++ src/lib/check-vies.ts
@@ -1,6 +1,38 @@
-import validateVat from 'validate-vat';
+//import validateVat from 'validate-vat';
+import validateVat, { CountryCodes, ViesValidationResponse} from 'validate-vat-ts';
 
-export default async (countryCode: string, vatId: string) : Promise<boolean> => new Promise((resolve, reject) => {
+enum ShortCountryCodes {
+  AT = CountryCodes.Austria,
+  BE = CountryCodes.Belgium,
+  BG = CountryCodes.Bulgaria,
+  HR = CountryCodes.Croatia,
+  CY = CountryCodes.Cyprus,
+  CZ = CountryCodes.CzechRepublic,
+  DK = CountryCodes.Denmark,
+  EE = CountryCodes.Estonia,
+  FI = CountryCodes.Finland,
+  FR = CountryCodes.France,
+  DE = CountryCodes.Germany,
+  EL = CountryCodes.Greece,
+  HU = CountryCodes.Hungary,
+  IE = CountryCodes.Ireland,
+  IT = CountryCodes.Italy,
+  LV = CountryCodes.Latvia,
+  LT = CountryCodes.Lithuania,
+  LU = CountryCodes.Luxembourg,
+  MT = CountryCodes.Malta,
+  NL = CountryCodes.Netherlands,
+  PL = CountryCodes.Poland,
+  PT = CountryCodes.Portugal,
+  RO = CountryCodes.Romania,
+  SK = CountryCodes.Slovakia,
+  SI = CountryCodes.Slovenia,
+  ES = CountryCodes.Spain,
+  SE = CountryCodes.Sweden,
+  XI = CountryCodes.NorthernIreland
+};
+
+export default async (countryCode: string, vatId: string) => {
   let vatNumber = vatId;
   const rgx = new RegExp(`^${countryCode}`, 'gi');
 
@@ -10,9 +42,10 @@ export default async (countryCode: string, vatId: string) : Promise<boolean> => 
 
   vatNumber = vatNumber.replace(/[^a-zA-Z0-9.]/g, ''); // can contain only numbers and letters
 
-  validateVat(countryCode, vatNumber, 120 * 1000, (err, validationInfo) => {
-    if (err) reject(err);
+  const validationInfo: ViesValidationResponse = await validateVat(ShortCountryCodes[countryCode], vatNumber);
 
-    resolve(validationInfo.valid);
-  });
-});
+  if (typeof validationInfo === 'undefined') {
+    throw Error("No validation info received");
+  }
+  return validationInfo.valid
+}
